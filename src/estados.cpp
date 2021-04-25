95cc0d57d1b148a03494dd398913f07b813cf0f9
Jesús de la Morena Duque <82572705+jdlmd@users.noreply.github.com>
2021-04-25 18:54:33 +0200

    Clase entrenador añadida, que me la dejé fuera

--- src/estados.cpp
+++ src/estados.cpp
@@ -11,6 +11,34 @@ Estados::~Estados() {
 
 }
 
-void Estados::resolveState(Pokemon* receiver) {
-    return;
+void Estados::resolveState(Pokemon *pokemon) {
+    switch (estado) {
+        case(NONE):
+            break;
+        case(PARALIZADO): // Caso de la parálisis completo
+            turnos--;
+            srand(time(NULL));
+
+            if(rand() % 100 <= 25) { // El pokemon no se mueve
+                mov = false;
+            }
+
+            if (!solve) {
+                pokemon->estadisticas_actuales.speed = pokemon->estadisticas_actuales.speed/2;
+                solve = true;
+            }
+
+            if (!turnos) { // Si se han acabado los turnos...
+                turnos = -1;
+                estado = NONE;
+                pokemon->estadisticas_actuales.speed = pokemon->estadisticas_actuales.speed*2;
+                solve = false;
+            }
+
+            break;
+        case(QUEMADO):
+            break;
+        default:
+            break;
+    }
 }
