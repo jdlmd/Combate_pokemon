93bd8508af58bc8402a1505c8443f9efb0727d65
Nerea Leon <82573517+nlm18@users.noreply.github.com>
2021-05-02 15:16:53 +0200

    pokemon.h comentado

--- src/pokemon.h
+++ src/pokemon.h
@@ -20,24 +20,23 @@ class Pokemon {
     public:
         Pokemon();
         ~Pokemon();
-        Pokemon(std::string _name,std::string _t1, std::string _t2, uint _level, struct stats _ivs, struct stats _evs, struct stats _base); // Terminar constructor
-        void setHP(uint newHP);
-        void addMove(Movimientos* move);
-        void removeMove(int i);
-        void setSecondaryType(Tipos _second);
-        uint getNumberMoves();
-        Movimientos* getMove(int i);
-        std::string getName();
-        int getHP();
-        int getHPtotal();
-        Tipos getPrimary();
-        Tipos getSecondary();
-//        Estado getState();
-        Estados* getStatePtr();
-        Estado getState();
-        uint getSpeed();
-        uint getLevel();
-        Tipo getType();
+        Pokemon(std::string _name,std::string _t1, std::string _t2, uint _level, struct stats _ivs, struct stats _evs, struct stats _base);
+        void setHP(uint newHP); // Fija la vida del pokemon
+        void addMove(Movimientos* move); // Añade un movimiento al pokemon
+        void removeMove(int i); // Elimina un movimiento del pokemon
+        void setSecondaryType(Tipos _second); // Fija el tipo secundario del pokemon
+        uint getNumberMoves(); // Devuelve el numero de movimientos que tiene el pokemon
+        Movimientos* getMove(int i); // Devuelve el puntero de un movimiento del pokemon
+        std::string getName(); // Devuelve el nombre del pokemon
+        int getHP(); // Devuelve la vida actual del pokemon
+        int getHPtotal(); // Devuelve la vida total del pokemon
+        Tipos getPrimary(); // Devuelve el tipo primario del pokemon
+        Tipos getSecondary(); // Devuelve el tipo secundario del pokemon
+        Estados* getStatePtr(); // Devuelve el puntero del estado del pokemon
+        Estado getState(); // Devuelve el estado del pokemon
+        uint getSpeed(); // Devuelve la velocidad del pokemon
+        uint getLevel(); // Devuelve el nivel del pokemon
+        Tipo getType(); // Devuelve el tipo primario del pokemon
 
     private:
         std::string name;
@@ -49,8 +48,8 @@ class Pokemon {
         uint level;
         Estados* state;
         Tipo type;
-        void calculateStatsFromBase(struct stats,struct stats,struct stats); // Implementar
-        void returnStats();
+        void calculateStatsFromBase(struct stats,struct stats,struct stats); // Calcula las estadisticas en base a los ivs, evs y los puntos base
+        void returnStats(); // Devuelve las estadisticas del pokemon a la normalidad, por si hubiera movimientos que potenciasen las estadisticas
 
     friend class Estados;
     friend class Movimientos;
